870d5f139fc98b822d0b99ced414a709812f36d7
ejbills <74191134+ejbills@users.noreply.github.com>
2021-10-10 17:10:47 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import discord
+
+from web_scraper import scrape
+
+token = ''
+
+link = 'https://www.google.com/search?q=new+grad+software+engineer+california&ibp=htl;jobs#htivrt=jobs&fpstate=tldetail&htichips=date_posted:3days'
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
+
+        if message.content == '!job':
+            jobs_returned = scrape(link, 3)
+
+            await message.channel.send(jobs_returned)
+            # for job in jobs_returned:
+            #     await message.channel.send(job)
+
+
+MyClient().run(token)
---
+++ web_scraper.py
@@ -0,0 +1,35 @@
+import re
+import requests
+
+from bs4 import BeautifulSoup
+
+def scrape(url, iterations):
+    counter = 0
+
+    jobs = []
+
+    session = requests.Session()
+    response = session.get(url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 ('
+                                                       'KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'})
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Finds game title element by "name" attribute
+    title = soup.findAll("div", {"class": re.compile('.*BjJfJf PUpOsf.*')}, limit=iterations)
+    company = soup.findAll("div", {"class": re.compile('.*vNEEBe.*')}, limit=iterations)
+    posted_date = soup.findAll("span", {"class": re.compile('.*SuWscb.*')}, limit=iterations)
+    links = soup.find_all("a", {"href": True})
+
+    for i in range(iterations):
+        jobs.append(title[i].text if title[i] else 'No job title found')
+        jobs.append(company[i].text if company[i] else 'No company found')
+        jobs.append(posted_date[i].text if posted_date[i] else 'No job post date found')
+
+    for link in links:
+        if link.get('title') and counter < iterations:
+            if 'Apply on' in link.get('title'):
+                jobs.append(link.get('href') if len(link.text) > 1 else 'No job link found')
+
+                counter += 1
+
+    return jobs
