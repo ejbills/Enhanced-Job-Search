c805300f1f41b6b52bcce4fd8dccaba23232b1cd
ejbills <74191134+ejbills@users.noreply.github.com>
2021-10-14 22:52:34 -0400

    general code cleanup and bug fixes
    * fixes duplicated results
    * fixes reassignment of variables

--- app.py
+++ app.py
@@ -1,44 +1,54 @@
+import copy
+
 from web_scrape.web_scraper import scrape
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
-job_link = 'https://www.google.com/search?q={}&ibp=htl;jobs#htivrt=jobs&fpstate=tldetail&htichips=date_posted:3days'
-jobs_list = []  # Stores jobs found in the form of Job constructor
+
 
 # Root domain
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
 # Execute query request with search() function with the given job title, then switch to /searched directory
 @app.route("/searched", methods=["POST"])
 def searched():
     query = request.form["job-title"]
+
     search(query)
-    return render_template("index.html", jobs_list=jobs_list)
+
+    temp_jobs_list = copy.copy(Job.jobs_list) # Handles stacked results being returned
+    Job.jobs_list.clear()
+
+    return render_template("index.html", jobs_list=temp_jobs_list)
+
 
 def search(job_title):
-    temp_link = job_title.replace(" ", "+")
-
-    jobs_returned = scrape(job_link.format(temp_link), 3)
+    jobs_returned = scrape(job_title, 10)
 
     for job in jobs_returned:
-        obj = Job(job[0], job[1], job[2], job[3])
+        obj = Job(job[0], job[1], job[2], job[3], Job.jobs_list)
         obj.add()
+
 
 class Job:
     company = ""
     job_title = ""
     date = ""
     url = ""
+    jobs_list = []
 
-    def __init__(self, company, job_title, date, url):
+    def __init__(self, company, job_title, date, url, jobs_list):
         self.company = company
         self.job_title = job_title
         self.date = date
         self.url = url
+        self.jobs_list = jobs_list
 
     def add(self):
-        jobs_list.append(self)
+        self.jobs_list.append(self)
+
 
 app.run()  # Execute Flask when running program, (CTRL + C) to exit
